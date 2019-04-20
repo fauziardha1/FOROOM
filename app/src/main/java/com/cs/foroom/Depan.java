92043c8db1e1c8b72cf84443fa9a48fc6f8996f6
fauziardha01 <fauziardasaputra@gmail.com>
2019-04-20 12:51:09 +0700

    Menambahkan activity_depan

---
+++ app/src/main/java/com/cs/foroom/Depan.java
@@ -0,0 +1,13 @@
+package com.cs.foroom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Depan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_depan);
+    }
+}
